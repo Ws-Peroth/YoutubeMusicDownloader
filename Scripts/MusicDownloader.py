5b1861a4a90b575603c89aa1092f2ee1f700775f
Ws-Peroth <a01033056809@gmail.com>
2023-02-10 10:06:11 +0900

    Update Python version 3.7.8 --> 3.9.8
    ERROR : Dosen't work on .py

--- Scripts/MusicDownloader.py
+++ Scripts/MusicDownloader.py
@@ -6,19 +6,19 @@ import os
 defaultPath = "C:/Users/User/OneDrive/바탕 화면/Files/music"
 
 class Downloader():
-    
+
     banlist = ['/', '\\', '*', '.', '?', '%', ':', '|', '"', "'", ]
 
     def __init__(self):
         self.link = ''
-        
+  
     def Start(self, link):
         self.link = link
-        
+
         print("Start")
         if( '/playlist?list=' in self.link):
             print("플레이 리스트 전체 다운로드")
-    
+
             playlist = Playlist(self.link)
             videolist = playlist.videos
             length = len(videolist)
@@ -26,7 +26,7 @@ class Downloader():
             if length == 0:
                 print('Playelist does not exist\n')
                 return
-            
+
             for i in range(0, length):
                 self.Download(videolist[i])
                 print(f"Downloading... ({i + 1}/{length})")
@@ -38,12 +38,12 @@ class Downloader():
                 print('This link is invalid.\n')
                 return
             self.Download(video)
-            
+
         print("Download Finish\n")
-    
+
     def ChangeFileType(self, filePath):
         files = glob.glob(filePath + "*.mp4")
-    
+
         for x in files:
             if not os.path.isdir(x):
                 filename = os.path.splitext(x)
@@ -59,12 +59,12 @@ class Downloader():
         for i in Downloader.banlist:
             if i in auth:
                 auth = auth.replace(i, '-')
-        
+
         path = f"{defaultPath}/{auth}/"
+        print(path)
         video.streams.filter(only_audio=onlyAudio).first().download(path)
         self.ChangeFileType(path)
-        
 
     def Finish(self):
-        input('press any key to exit process...')
+        input('press any key...')
         exit()
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,11 +1,15 @@
 import MusicDownloader as md
 
-print('프로그램을 종료하려면 exit 를 입력하세요')
+try:
+    print('프로그램을 종료하려면 exit 를 입력하세요')
 
-while(True):
-    link = input('영상 링크를 입력하시오 : ')
-    video = md.Downloader()
+    while(True):
+        link = input('영상 링크를 입력하시오 : ')
+        video = md.Downloader()
     
-    if(link == 'exit'): video.Finish()
-    video.Start(link)
-    
+        if(link == 'exit'): video.Finish()
+        video.Start(link)
+except Exception as e:
+    print(f"ERROR: {e}")
+    print('프로그램을 종료하려면 아무 키나 누르시오...')
+    input()
